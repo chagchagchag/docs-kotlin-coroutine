6d7990455a3003dc33309b8435d3803ac809dbc0
chagchagchag <chagchagchag.dev@gmail.com>
2024-03-14 09:03:35 +0900

    first status

---
+++ theme.config.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { DocsThemeConfig } from 'nextra-theme-docs'
+
+const config: DocsThemeConfig = {
+  logo: <span>Docs Kotlin Coroutine</span>,
+  project: {
+    link: 'https://github.com/chagchagchag/docs-kotlin-coroutine',
+  },
+  docsRepositoryBase: 'https://github.com/chagchagchag/docs-kotlin-coroutine',
+  footer: {
+    text: 'Nextra Docs Template',
+  },
+}
+
+export default config
